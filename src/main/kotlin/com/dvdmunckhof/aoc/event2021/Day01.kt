2f7ec8a33c1ffc061f57723fab997c62c29ab15d
Dennis van den Munckhof <dennis_vd_munckhof@live.nl>
2021-12-02 16:56:18 +0100

    2021 Day 1: Sonar Sweep - improve solution

--- src/main/kotlin/com/dvdmunckhof/aoc/event2021/Day01.kt
+++ src/main/kotlin/com/dvdmunckhof/aoc/event2021/Day01.kt
@@ -2,14 +2,11 @@ package com.dvdmunckhof.aoc.event2021
 
 class Day01(private val input: List<Int>) {
 
-    fun solvePart1(): Int {
-        return input.windowed(2).count { it[0] < it [1] }
-    }
+    fun solvePart1(): Int = solve(2)
 
-    fun solvePart2(): Int {
-        return input.windowed(3)
-            .map { it.sum() }
-            .windowed(2)
-            .count { it[0] < it [1] }
+    fun solvePart2(): Int = solve(4)
+
+    private fun solve(step: Int): Int {
+        return input.windowed(step).count { it.first() < it.last() }
     }
 }
