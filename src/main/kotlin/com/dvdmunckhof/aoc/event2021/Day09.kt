f99ccbf4bf2ecaa220ee180e9437771cc9c88575
Dennis van den Munckhof <dennis_vd_munckhof@live.nl>
2021-12-09 16:20:29 +0100

    2021 Day 9: Smoke Basin - part 1 - improve solution

--- src/main/kotlin/com/dvdmunckhof/aoc/event2021/Day09.kt
+++ src/main/kotlin/com/dvdmunckhof/aoc/event2021/Day09.kt
@@ -4,20 +4,13 @@ class Day09(input: List<String>) {
     private val grid = input.map { row -> row.map(Char::digitToInt) }
 
     fun solvePart1(): Int {
-        val directions = Direction.values().toMutableList()
         val results = grid.flatMapIndexed { x, row ->
-            val rowDirections = when(x) {
-                0 -> directions - Direction.NORTH
-                grid.lastIndex -> directions - Direction.SOUTH
-                else -> directions
-            }
             row.filterIndexed { y, height ->
-                val locationDirections = when (y) {
-                    0 -> rowDirections - Direction.WEST
-                    row.lastIndex -> rowDirections - Direction.EAST
-                    else -> rowDirections
-                }
-                locationDirections.all { d -> grid[x + d.north][y + d.east] > height }
+                val north = x == 0 || grid[x - 1][y] > height
+                val east = y == row.lastIndex || row[y + 1] > height
+                val south = x == grid.lastIndex || grid[x + 1][y] > height
+                val west = y == 0 || row[y - 1] > height
+                north && east && south && west
             }
         }
         return results.sumOf { it + 1 }
@@ -25,9 +18,5 @@ class Day09(input: List<String>) {
 
     fun solvePart2(): Int {
         error("No solution found")
-    }
-
-    private enum class Direction(val north: Int, val east: Int) {
-        NORTH(-1, 0), EAST(0, 1), SOUTH(1, 0), WEST(0, -1);
     }
 }
