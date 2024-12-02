bdac0a51d13eaa79981d3a857c27429ffa325728
Dennis van den Munckhof <3891060+dvdmunckhof@users.noreply.github.com>
2024-12-02 17:11:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Advent of Code
+
+![License](https://img.shields.io/github/license/dvdmunckhof/advent-of-code)
+
+Solutions for [Advent of Code 2024](https://adventofcode.com/) in Kotlin.
---
+++ src/main/kotlin/com/dvdmunckhof/aoc/common/PuzzleInput.kt
@@ -0,0 +1,21 @@
+package com.dvdmunckhof.aoc.common
+
+import java.io.Reader
+import java.nio.file.Paths
+import kotlin.io.path.reader
+
+@JvmInline
+value class PuzzleInput(val fileName: String) {
+
+    fun reader(): Reader {
+        return Paths.get(fileName).reader()
+    }
+
+    fun readString(): String {
+        return reader().use(Reader::readText).trimEnd()
+    }
+
+    fun readLines(): List<String> {
+        return reader().use(Reader::readLines)
+    }
+}
